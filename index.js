532df39416fafc15ef04afcd4ca273a6135f0c76
Tom <eizandev@gmail.com>
2021-12-13 22:51:51 +0100

    board now working

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
 const board = document.getElementById('gameCanvas')
 const boardContext = gameCanvas.getContext("2d");
 
-let snake = [{ x: 350, y: 350 }, { x: 340, y: 350 }, { x: 330, y: 350 }, { x: 320, y: 350 }, { x: 310, y: 350 }, { x: 300, y: 350 },]
+let snake = [
+    { x: 350, y: 350 },
+    { x: 340, y: 350 },
+    { x: 330, y: 350 },
+    { x: 320, y: 350 },
+    { x: 310, y: 350 },
+    { x: 300, y: 350 }
+]
 
+// Function to get all the snake parts positions
 function drawSnakeParts(snakePos) {
-    boardContext.fillStyle = 'lightblue';
+    boardContext.fillStyle = '#8bc34a';
     boardContext.strokestyle = 'darkblue';
     boardContext.fillRect(snakePos.x, snakePos.y, 10, 10); //Draws a filled rectangle.
     boardContext.strokeRect(snakePos.x, snakePos.y, 10, 10); //Draws a rectangular outline. 
 }
 
 function drawSnake() {
-    snake.forEach(part => {
-        drawSnakeParts(part);
-    });
+    snake.forEach(drawSnakeParts);
 }
 
-snake.forEach(part => {
-    console.log(part.x);
-})
-drawSnakeParts();
+// Function called to clear the board every time
+function clearCanvas() {
+    boardContext.fillStyle = 'white';
+    boardContext.strokestyle = 'black';
+    // Draw a "filled" rectangle to cover the entire canvas
+    boardContext.fillRect(0, 0, board.width, board.height);
+    // Draw a "border" around the entire canvas
+    boardContext.strokeRect(0, 0, board.width, board.height);
+}
+
+clearCanvas()
 drawSnake();
+
+
+
+
 
 
