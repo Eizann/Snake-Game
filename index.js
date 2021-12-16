54c16330b9aa7ad8f0ddbe377c326e7a2cdebc6c
Tom <eizandev@gmail.com>
2021-12-16 14:23:42 +0100

    design left

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-const board = document.getElementById('gameCanvas')
+const board = document.getElementById('gameCanvas');
 const boardContext = gameCanvas.getContext("2d");
+const newGameButton = document.getElementById('new-game');
+const scoreBoard = document.getElementById('score');
+let myMusic;
+myMusic = new sound("snakemusic.mp3");
+document.querySelector('audio').loop = true;
+myMusic.play();
+
 
 let snake = [
-    { x: 350, y: 350 },
-    { x: 340, y: 350 },
-    { x: 330, y: 350 },
-    { x: 320, y: 350 },
-    { x: 310, y: 350 },
-    { x: 300, y: 350 }
+    { x: 200, y: 200 },
+    { x: 190, y: 200 },
+    { x: 180, y: 200 },
+    { x: 170, y: 200 },
+    { x: 160, y: 200 },
+    { x: 150, y: 200 }
 ]
 
 let changing_direction = false;
 let horizontalVelocity = 10;
 let verticalVelocity = 0;
+let score = 0;
+let food_x;
+let food_y;
 
 // Function to get all the snake parts positions
 function drawSnakeParts(snakePos) {
@@ -22,7 +32,7 @@ function drawSnakeParts(snakePos) {
     boardContext.strokeRect(snakePos.x, snakePos.y, 10, 10); //Draws a rectangular outline. 
 }
 
-//Function to draw the snake 
+// Function to draw the snake 
 function drawSnake() {
     snake.forEach(drawSnakeParts);
 }
@@ -30,26 +40,32 @@ function drawSnake() {
 function move_snake() {
     const head = { x: snake[0].x + horizontalVelocity, y: snake[0].y + verticalVelocity };
     snake.unshift(head);
-    snake.pop();
+    const has_eaten_food = snake[0].x === food_x && snake[0].y === food_y;
+    if (has_eaten_food) {
+        score += 10;
+        scoreBoard.innerHTML = `Score : ${score}`;
+        gen_food();
+    } else {
+        // Remove the last part of snake body
+        snake.pop();
+    }
 }
 
-// Function called to clear the board every time
+// Function called to clear all previous positions of the snake on the board
 function clearCanvas() {
     boardContext.fillStyle = 'white';
     boardContext.strokestyle = 'black';
-    // Draw a "filled" rectangle to cover the entire canvas
     boardContext.fillRect(0, 0, board.width, board.height);
-    // Draw a "border" around the entire canvas
-    boardContext.strokeRect(0, 0, board.width, board.height);
 }
 
+// Function to change the direction of the snake
 function change_direction(event) {
     const left = 65;
     const right = 68;
     const up = 87;
     const down = 83;
 
-    /* if (changing_direction) return; */
+    if (changing_direction) return;
     changing_direction = true;
     const keyPressed = event.keyCode;
     const goingUp = verticalVelocity === -10;
@@ -78,29 +94,93 @@ function change_direction(event) {
     }
 }
 
+// Function to check is the game has ended or not
 function has_game_ended() {
     for (let i = 4; i < snake.length; i++) {
-        if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true
+        if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true;
     }
     const hitLeftWall = snake[0].x < 0;
     const hitRightWall = snake[0].x > board.width - 10;
-    const hitToptWall = snake[0].y & lt; 0;
+    const hitToptWall = snake[0].y < 0;
     const hitBottomWall = snake[0].y > board.height - 10;
 
     return hitLeftWall || hitRightWall || hitToptWall || hitBottomWall;
 }
 
-function main() {
+// Functions to generate a random food on the map
+function random_food(min, max) {
+    return Math.round((Math.random() * (max - min) + min) / 10) * 10;
+}
 
-    /* if (has_game_ended()) return; */
+function gen_food() {
+    food_x = random_food(0, board.width - 10);
+    food_y = random_food(0, board.height - 10);
+    snake.forEach(function has_snake_eaten_food(part) {
+        const has_eaten = part.x == food_x && part.y == food_y;
+        if (has_eaten) gen_food();
+    });
+}
+
+
+// Function to draw the food
+function drawFood() {
+    boardContext.fillStyle = 'lightgreen';
+    boardContext.strokestyle = 'darkgreen';
+    boardContext.fillRect(food_x, food_y, 10, 10);
+    boardContext.strokeRect(food_x, food_y, 10, 10);
+}
+
+// Function we're calling at each state of the game
+function main() {
+    if (has_game_ended()) return;
     changing_direction = false;
     setTimeout(function onTick() {
         clearCanvas();
+        drawFood();
         move_snake();
         drawSnake();
         main();
     }, 100);
 }
 
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function () {
+        this.sound.play();
+    }
+    this.stop = function () {
+        this.sound.pause();
+    }
+}
+
+function newGame() {
+    snake = [
+        { x: 200, y: 200 },
+        { x: 190, y: 200 },
+        { x: 180, y: 200 },
+        { x: 170, y: 200 },
+        { x: 160, y: 200 },
+        { x: 150, y: 200 }
+    ]
+    score = 0;
+    scoreBoard.innerHTML = `Score : ${score}`;
+    food_x;
+    food_y;
+    changing_direction = false;
+    horizontalVelocity = 10;
+    verticalVelocity = 0;
+    main();
+    gen_food();
+}
+
 main();
+gen_food();
 document.addEventListener("keydown", change_direction);
+newGameButton.addEventListener("click", newGame);
+
+
